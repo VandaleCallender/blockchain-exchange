471674d574f75ecf2799d21fc7f5416472afee13
Vandale Callender <44715177+VandaleCallender@users.noreply.github.com>
2021-04-24 02:34:35 -0400

    Exchange
    Start building out crypto currency exchange smart contract.
    
    Smart contract that will handle all the behavior

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,14 @@
+// Deposit & Withdraw Funds
+// Manage Orders - Make or Cancel
+// Handle Trades - Charge fees
+
+//TODO:
+//[] Deposit Ether
+//[] Withdraw Ether
+//[] Deposit Tokens
+//[] Withdraw Tokens
+//[] Check Balances
+//[] Make Order
+//[] Cancel Order
+//[] Fill Order
+//[] Charge Fees
